0773b9e9a5b8fcf9b45e08465b2983ea674a80cd
Kashish <kashishagrawal039@gmail.com>
2024-02-09 00:59:44 +0530

    Modify file be adding Function in it

--- Automation/Automation.py
+++ Automation/Automation.py
@@ -1,27 +1,36 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart,Reference
 
-# load the Transtion sheet
-wb=xl.load_workbook("Transaction.xlsx")
-sheet=wb['Sheet1']
-# WAy 1
-# cell=sheet['a1']
-# Way 2
-# cell=sheet.cell(1,1);
-# print(sheet.max_row);
-for i in range (2,sheet.max_row+1):
-    cell=sheet.cell(i,3);
-    corect_price=cell.value*0.9;
-    corrected_cell=sheet.cell(i,4)
-    corrected_cell.value=corect_price;
+def proccess_workbook(filename):
 
-values=Reference(sheet,
-          min_row=2,
-          max_row=sheet.max_row,
-          min_col=4,
-          max_col=4)
+    # load the Transtion sheet
+    wb=xl.load_workbook(filename)
+    sheet=wb['Sheet1']
 
-chart=BarChart()
-chart.add_data(values)
-sheet.add_chart(chart,'e2')
-wb.save("Trans2.xlsx")
+
+    # WAy 1
+    # cell=sheet['a1']
+    # Way 2
+    # cell=sheet.cell(1,1);
+    # print(sheet.max_row);
+
+    for i in range (2,sheet.max_row+1):
+        cell=sheet.cell(i,3);
+        corect_price=cell.value*0.9;
+        corrected_cell=sheet.cell(i,4)
+        corrected_cell.value=corect_price;
+
+    values=Reference(sheet,
+              min_row=2,
+              max_row=sheet.max_row,
+              min_col=4,
+              max_col=4)
+
+    chart=BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart,'e2')
+
+    wb.save(filename)
+
+
+proccess_workbook("Transaction.xlsx")
